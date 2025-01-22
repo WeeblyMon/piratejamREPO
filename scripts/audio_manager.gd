b86b134ce86e66213315fd34abf881dd179b9d38
Dev-Trick <michaelcaldwell1988@gmail.com>
2025-01-22 11:58:01 -0600

    Created Audio Manager and added SFX to assets

---
+++ scripts/audio_manager.gd
@@ -0,0 +1,28 @@
+extends Node
+
+@onready var scream_2: AudioStreamPlayer = $Scream_2
+@onready var scream_1: AudioStreamPlayer = $Scream_1
+@onready var reload_1: AudioStreamPlayer = $Reload_1
+@onready var pain_2: AudioStreamPlayer = $Pain_2
+@onready var pain_1: AudioStreamPlayer = $Pain_1
+@onready var mission_failed_1: AudioStreamPlayer = $Mission_Failed_1
+@onready var mission_complete_1: AudioStreamPlayer = $Mission_Complete_1
+@onready var menu_navigation_ding_1: AudioStreamPlayer = $Menu_Navigation_Ding_1
+@onready var menu_navigation_confirm_1: AudioStreamPlayer = $Menu_Navigation_Confirm_1
+@onready var gun_jam_1: AudioStreamPlayer = $Gun_Jam_1
+@onready var gunshot_1: AudioStreamPlayer = $Gunshot_1
+@onready var grunt_2: AudioStreamPlayer = $Grunt_2
+@onready var grunt_1: AudioStreamPlayer = $Grunt_1
+@onready var footsteps_wood_1_2: AudioStreamPlayer = $Footsteps_Wood_1_2
+@onready var footsteps_wood_1_1: AudioStreamPlayer = $Footsteps_Wood_1_1
+@onready var footsteps_asphalt_1_2: AudioStreamPlayer = $Footsteps_Asphalt_1_2
+@onready var footsteps_asphalt_1_1: AudioStreamPlayer = $Footsteps_Asphalt_1_1
+@onready var enemy_hit_and_blood_1: AudioStreamPlayer = $Enemy_Hit_And_Blood_1
+@onready var enemy_hit_1: AudioStreamPlayer = $Enemy_Hit_1
+@onready var enemy_hit_1_1: AudioStreamPlayer = $Enemy_Hit_1_1
+@onready var enemy_hit_1_2: AudioStreamPlayer = $Enemy_Hit_1_2
+@onready var death_1: AudioStreamPlayer = $Death_1
+@onready var collision_ping_1: AudioStreamPlayer = $Collision_Ping_1
+@onready var casing_drop_1: AudioStreamPlayer = $Casing_Drop_1
+@onready var bullet_slow_mo_1: AudioStreamPlayer = $Bullet_Slow_Mo_1
+@onready var bullet_impact_1: AudioStreamPlayer = $Bullet_Impact_1
