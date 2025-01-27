6a575e91abc26cdb2a6c56b5c383dd87e772abc1
WeeblyMon <80261078+WeeblyMon@users.noreply.github.com>
2025-01-27 21:12:28 +0000

    Imported Audio Files

--- scripts/managers/audio_manager.gd
+++ scripts/managers/audio_manager.gd
@@ -11,43 +11,50 @@ var music_tracks: Dictionary = {}
 
 @export var audio_files: Dictionary = {
 	"sfx": {
-		"scream_2": "res://assets/audio/sfx/Scream_2.mp3",
 		"scream_1": "res://assets/audio/sfx/Scream_1.mp3",
+		"scream_2": "res://assets/audio/sfx/Scream_2.mp3",
 		"reload_1": "res://assets/audio/sfx/Reload_1.mp3",
-		"pain_2": "res://assets/audio/sfx/Pain_2.mp3",
 		"pain_1": "res://assets/audio/sfx/Pain_1.mp3",
+		"pain_2": "res://assets/audio/sfx/Pain_2.mp3",
 		"mission_failed_1": "res://assets/audio/sfx/Mission_Failed_1.mp3",
 		"mission_complete_1": "res://assets/audio/sfx/Mission_Complete_1.mp3",
 		"menu_navigation_ding_1": "res://assets/audio/sfx/Menu_Navigation_Ding_1.mp3",
 		"menu_navigation_confirm_1": "res://assets/audio/sfx/Menu_Navigation_Confirm_1.mp3",
+		"menu_navigation_back_1": "res://assets/audio/sfx/Menu_Navigation_Back_1.mp3",
 		"gun_jam_1": "res://assets/audio/sfx/Gun_Jam_1.mp3",
 		"gunshot_1": "res://assets/audio/sfx/Gunshot_1.mp3",
 		"handgun_shot": "res://assets/audio/sfx/pistol-shot.mp3",
 		"rifle_shot": "res://assets/audio/sfx/sniper-rifle.mp3",
 		"shotgun_shot": "res://assets/audio/sfx/shotgun-firing.mp3",
-		"grunt_2": "res://assets/audio/sfx/Grunt_2.mp3",
 		"grunt_1": "res://assets/audio/sfx/Grunt_1.mp3",
-		"footsteps_wood_1_2": "res://assets/audio/sfx/Footsteps_Wood_1.2.mp3",
+		"grunt_2": "res://assets/audio/sfx/Grunt_2.mp3",
 		"footsteps_wood_1_1": "res://assets/audio/sfx/Footsteps_Wood_1.1.mp3",
-		"footsteps_asphalt_1_2": "res://assets/audio/sfx/Footsteps_Asphalt_1.2.mp3",
+		"footsteps_wood_1_2": "res://assets/audio/sfx/Footsteps_Wood_1.2.mp3",
 		"footsteps_asphalt_1_1": "res://assets/audio/sfx/Footsteps_Asphalt_1.1.mp3",
-		"enemy_hit_and_blood_1": "res://assets/audio/sfx/Enemy_Hit_And_Blood_1.mp3",
+		"footsteps_asphalt_1_2": "res://assets/audio/sfx/Footsteps_Asphalt_1.2.mp3",
 		"enemy_hit_1": "res://assets/audio/sfx/Enemy_Hit_1.mp3",
 		"enemy_hit_1_1": "res://assets/audio/sfx/Enemy_Hit_1.1.mp3",
 		"enemy_hit_1_2": "res://assets/audio/sfx/Enemy_Hit_1.2.mp3",
+		"enemy_hit_and_blood_1": "res://assets/audio/sfx/Enemy_Hit_And_Blood_1.mp3",
 		"death_1": "res://assets/audio/sfx/Death_1.mp3",
 		"collision_ping_1": "res://assets/audio/sfx/Collision_Ping_1.mp3",
 		"casing_drop_1": "res://assets/audio/sfx/Casing_Drop_1.mp3",
 		"bullet_slow_mo_1": "res://assets/audio/sfx/Bullet_Slow_Mo_1.mp3",
 		"bullet_impact_1": "res://assets/audio/sfx/Bullet_Impact_1.mp3",
-		
+		"bullet_steering_1": "res://assets/audio/sfx/Bullet_Steering_1.mp3",
+		"crowd_chatter_1": "res://assets/audio/sfx/Crowd_Chatter_1.mp3",
+		"explosion_1": "res://assets/audio/sfx/Explosion_1.mp3",
+		"explosion_2": "res://assets/audio/sfx/Explosion_2.mp3",
+		"sanity_heartbeat_1": "res://assets/audio/sfx/Sanity_Heartbeat_1.mp3",
+		"siren_passing_by_1": "res://assets/audio/sfx/Siren_Passing_By_1.mp3",
+		"tv_static_1": "res://assets/audio/sfx/TV_Static_1.mp3",
+		"wood_break_1": "res://assets/audio/sfx/Wood_Break_1.mp3",
+		"wood_break_2": "res://assets/audio/sfx/Wood_Break_2.mp3"
 	},
 	"music": {
-		"music_track_1": "res://assets/audio/music/Normal_Theme_Sketch_1.1.mp3",
-		# Add more Music file paths here...
+		"music_track_1": "res://assets/audio/music/Normal_Theme_Sketch_1.1.mp3"
 	}
 }
-
 func _ready() -> void:
 	for sfx_name in audio_files["sfx"].keys():
 		var player = AudioStreamPlayer.new()
